5f5b8ff9759950479eddf1f0e9bdda7811b8797e
CKamande <mungaigpi@gmail.com>
2025-03-07 22:14:46 +0300

    added Srabbleletters.js - updated board.js code

---
+++ src/Scrabbleletter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useDrag } from "react-dnd";
+
+const ScrabbleLetter = ({ id, letter, position }) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: "TILE",
+    item: { id, letter, position },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <div ref={drag} className="letter-tile" style={{ opacity: isDragging ? 0.5 : 1 }}>
+      {letter}
+      <span className="tile-score">{letter ? getLetterScore(letter) : ""}</span>
+    </div>
+  );
+};
+
+const getLetterScore = (letter) => {
+  const scores = { A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10 };
+  return scores[letter] || 0;
+};
+
+export default ScrabbleLetter;
